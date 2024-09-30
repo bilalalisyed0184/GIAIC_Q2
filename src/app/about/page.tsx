625b9e90aefe39224c6cd57add1b7f037334dd14
bilalalisyed0184 <156933641+bilalalisyed0184@users.noreply.github.com>
2024-09-30 10:33:54 -0700

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+function Aboutpage() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    ); 
+  }
+
+  export default Aboutpage
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+function Homepage() {
+  return (
+    <div>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+    
+    <h1>Home Page</h1>
+    <h1>Hello World</h1>
+    <br/>
+    <h2>from GIAIC</h2>
+    </div>
+  )
+}
+
+export default Homepage
